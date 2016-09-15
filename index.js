b25ac01a4f10bf80095b84084c96d1e4741926bf
Andrew <atandiawan@gmail.com>
2016-09-15 15:49:40 +0700

    Fixed, except Uniqueness

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let model = require("./models/index")
 let repl = require("repl")
 let Sequelize = require("sequelize")
 
-// model.Student.create({first_name: "Halo", last_name: "Dakas", age:10, phone: "013415801348", email: "unieuqa@gmail.com"})
+ model.Student.create({first_name: "Halos", age:10, phone: "013415801348", email: "unieuqdfkajlaaaa@gmail.com", address: "jl jalan"})
 // model.Student.create({first_name: "lo", last_name: "Jas", age:20, phone: "01543801348", email: "uieuq@gmail.com"})
 // model.Student.create({first_name: "Hao", last_name: "Dakas", age:17, phone: "03213415801348", email: "uniq@gmail.com"})
 
@@ -12,7 +12,7 @@ let getName = function(){
   model.Student.findAll().then(function(students){
     let string = []
     for(let i in students){
-      console.log(students[i].first_name+" "+students[i].last_name)
+      console.log(students[i].first_name)
     }
   })
 }
@@ -21,12 +21,10 @@ let getAge = function(){
   model.Student.findAll().then(function(students){
     let string = []
     for(let i in students){
-      console.log(students[i].first_name+" "+students[i].last_name+" berumur "+ students[i].age)
+      console.log(students[i].first_name+" berumur "+ students[i].age)
     }
   })
 }
-
-queryInterface.addColumn("Student","address",Sequelize.STRING)
 
 let replServer = repl.start({prompt:">"})
 replServer.context.getAge = getAge
--- models/student.js
+++ models/student.js
@@ -2,16 +2,45 @@
 module.exports = function(sequelize, DataTypes) {
   var Student = sequelize.define('Student', {
     first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
-    age: DataTypes.INTEGER,
-    email: DataTypes.STRING,
-    phone: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
+    age: {type: "STRING",
+    validate:{
+      min: 5,
+      // msg: "Must be older than 5"
     }
-  });
-  return Student;
+  },
+
+  phone: {type: "STRING",
+  validate:{
+    isNumeric: {msg:"phone must be numeric only"},
+    isLong: function(value){
+      if(value.length<10){
+        throw new Error('Phone number must be longer than 10 digits')
+      }
+    }}
+  },
+
+  address: DataTypes.STRING,
+
+  email: {type: "STRING",
+  validate:{
+    isEmail: {msg:"email must be in correct format"},
+    isUnique: function(value){
+      Student.findAll().then(function(students){
+        for(let i in students){
+          if(value == students[i].email){
+            throw new Error('Email is already in use')
+          }
+        }
+      })
+    }
+  }
+  },
+}, {
+  classMethods: {
+    associate: function(models) {
+      // associations can be defined here
+    }
+  }
+});
+return Student;
 };
