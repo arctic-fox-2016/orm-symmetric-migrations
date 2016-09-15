7785a70c75fe212b642d191b36381db1766f0128
Ahyana Rizky Pratama & Septhianto Diga <pair+ahyanarizky.digachandra@hacktiv8.com>
2016-09-15 19:47:42 +0700

    commit

---
+++ models/students.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Students = sequelize.define('Students', {
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+    email: DataTypes.STRING,
+    telp: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Students;
+};
