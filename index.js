6069260e91d1f89103db3fed7b54185ff2843427
Andrew <atandiawan@gmail.com>
2016-09-15 14:48:21 +0700

    Initialize

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-'use strict'
+"use strict"
 
+let model = require("./models/index")
+let repl = require("repl")
+
+model.Student.create({first_name: "halossa", last_name: "Dakas", age:10, phone: "013415801348", email: "unieuq@gmail.com"})
+
+let getName = function(){
+  model.Student.findAll().then(function(students){
+    let string = []
+    for(let i in students){
+      console.log(students[i].first_name+" "+students[i].last_name)
+    }
+  })
+}
+
+let getAge = function(){
+  model.Student.findAll().then(function(students){
+    let string = []
+    for(let i in students){
+      console.log(students[i].first_name+" "+students[i].last_name+" berumur "+ students[i].age)
+    }
+  })
+}
+
+let replServer = repl.start({prompt:">"})
+replServer.context.getAge = getAge
