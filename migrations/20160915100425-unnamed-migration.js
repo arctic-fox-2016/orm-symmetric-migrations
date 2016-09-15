3c3799e9d362d86f9eec3d2b9c70e7a55954625b
Ahmad Ahmadi & Ryan Andhaka <pair+ahmadahmadi14.ryanandhaka@hacktiv8.com>
2016-09-15 17:13:51 +0700

    ryan-ahmad

--- migrations/20160915100425-unnamed-migration.js
+++ migrations/20160915100425-unnamed-migration.js
@@ -9,13 +9,12 @@ module.exports = {
       Example:
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
-  return queryInterface.removeColumn(
-  'Students',
-  'last_name'
-)
-},
-  down: function (queryInterface, Sequelize) {
+    return queryInterface.renameColumn(
+      'Students',
+      'full_name')
+  },
 
+  down: function (queryInterface, Sequelize) {
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
